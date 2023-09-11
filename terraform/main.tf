61386065dec125cd774953fe042b05028d0e19e1
Genesis Alvarez <genesisalvarez@MacBook-Pro.localdomain>
2023-09-11 15:41:02 -0400

    separating network configurations from infrastructure resources

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,57 +3,6 @@ data "google_project" "project" {
 }
 
 #---------------------------
-# VPC NETWORKS
-#---------------------------
-
-resource "google_compute_network" "network" {
-  name = var.network
-  auto_create_subnetworks = true
-}
-
-# Define a firewall rule for xroad
-resource "google_compute_firewall" "rules" {
-  name    = "allow-xroad"
-  network = google_compute_network.network.self_link
-
-  allow {
-    protocol = "tcp"
-    ports    = ["5500", "5577", "443", "4000"]
-  }
-
-  source_ranges = ["0.0.0.0/0"] # Allow access from anywhere (adjust for production)
-}
-
-resource "google_compute_firewall" "ssh" {
-  name    = "allow-ssh"
-  network = google_compute_network.network.self_link
-
-  allow {
-    protocol = "tcp"
-    ports    = ["22"]
-  }
-
-  source_ranges = ["0.0.0.0/0"] # Allow access from anywhere (adjust for production)
-}
-
-resource "google_compute_firewall" "allow-outgoing" {
-  name    = "allow-outgoing"
-  network = google_compute_network.network.self_link
-
-  allow {
-    protocol = "all"
-  }
-
-  source_ranges = ["0.0.0.0/0"]  
-  direction     = "EGRESS"       
-}
-
-resource "google_compute_address" "static_ip" {
-  name   = "xroad-static-ip"
-  region = var.region 
-}
-
-#---------------------------
 # Disk
 #---------------------------
 resource "google_compute_disk" "data_disk" {
